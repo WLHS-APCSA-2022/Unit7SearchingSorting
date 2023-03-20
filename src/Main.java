f51d4eb902004b02a99baefbe4dfe3beda5ac3f1
Brandon Smith <smithb@wlwv.k12.or.us>
2023-03-20 09:14:50 -0700

    student solution 1

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println(numList);
         Collections.sort(numList);
         System.out.println(numList);
-
+        int location = binarySearch(numList,25);
     }
     //write the binary search to look for 25
     //find the middle
@@ -24,5 +24,36 @@ public class Main {
     //else get rid of middle and elements to left
     //repeat until you are left with one element
     //if that element is target, return it, else return -1
+
+    /**
+     * Uses a binary search to find an integer in a list
+     *
+     * @param list list you are searching
+     * @param target number you want to find
+     * @return index of the target or -1 if it is not found
+     */
+    public static int binarySearch(ArrayList<Integer> list, int target){
+        int middle;
+        double index;
+        int length = list.size();
+        if(length%2 == 1){
+            index = length/2.0;
+            middle = list.get(length/2);
+        } else{
+            index = length/2.0 - 0.5;
+            middle = (list.get(length/2-1)+list.get(length/2))/2;
+        }
+
+        if(target == middle){
+            return (int)(index);
+        }
+        if(target < middle){
+
+        } else{
+
+        }
+
+        return -1;
+    }
 
 }
