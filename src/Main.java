c185f4e776166a3f0cc209875b8d81ba0bb0bfa6
Brandon Smith <smithb@wlwv.k12.or.us>
2023-03-20 09:03:45 -0700

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,13 @@ public class Main {
         Collections.sort(numList);
         System.out.println(numList);
 
-        //write the binary search to look for 25
-        //find the middle
-        //is target less than or greater than middle
-        //if less, get rid of middle and elements to right
-        //else get rid of middle and elements to left
-        //repeat until you are left with one element
-        //if that element is target, return it, else return -1
     }
+    //write the binary search to look for 25
+    //find the middle
+    //is target less than or greater than middle
+    //if less, get rid of middle and elements to right
+    //else get rid of middle and elements to left
+    //repeat until you are left with one element
+    //if that element is target, return it, else return -1
+
 }
